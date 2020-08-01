4e9e42083fdb6088b79ec12978e96f79d952b328
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-01 18:58:06 -0300

    move config to mod.

---
+++ src/config_reader.rs
@@ -0,0 +1,27 @@
+use rdkafka::config::ClientConfig;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    bootstrap_servers: String,
+    security_protocol: String,
+    sasl_mechanism: String,
+    sasl_username: String,
+    sasl_password: String,
+    topic: String,
+}
+
+pub fn read_config() -> (ClientConfig, String) {
+    let mut kafka_config = ClientConfig::new();
+    match envy::from_env::<Config>() {
+        Ok(config) => {
+            kafka_config.set("bootstrap.servers", &config.bootstrap_servers);
+            kafka_config.set("security.protocol", &config.security_protocol);
+            kafka_config.set("sasl.mechanism", &config.sasl_mechanism);
+            kafka_config.set("sasl.username", &config.sasl_username);
+            kafka_config.set("sasl.password", &config.sasl_password);
+            (kafka_config, config.topic)
+        }
+        Err(error) => panic!("Couldn't read config ({})", error),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
-use rdkafka::config::ClientConfig;
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Config {
-    bootstrap_servers: String,
-    security_protocol: String,
-    sasl_mechanism: String,
-    sasl_username: String,
-    sasl_password: String,
-    topic: String,
-}
-
-fn read_config() -> (ClientConfig, String) {
-    let mut kafka_config = ClientConfig::new();
-    match envy::from_env::<Config>() {
-        Ok(config) => {
-            kafka_config.set("bootstrap.servers", &config.bootstrap_servers);
-            kafka_config.set("security.protocol", &config.security_protocol);
-            kafka_config.set("sasl.mechanism", &config.sasl_mechanism);
-            kafka_config.set("sasl.username", &config.sasl_username);
-            kafka_config.set("sasl.password", &config.sasl_password);
-            (kafka_config, config.topic)
-        }
-        Err(error) => panic!("Couldn't read LANG ({})", error),
-    }
-}
+mod config_reader;
 
 fn main() {
-    println!("Config baby {:?}", read_config().0);
-    println!("TOPICO {:?}", read_config().1);
+    let config = config_reader::read_config();
+    println!("Config baby {:?}", config.0);
+    println!("TOPICO {:?}", config.1);
 }
