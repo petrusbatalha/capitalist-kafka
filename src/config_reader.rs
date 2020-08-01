ed9bc3e0c4a85aef08b31ebf6b5853f0c5fdaba6
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-01 20:37:13 -0300

    change config to toml file.

--- src/config_reader.rs
+++ src/config_reader.rs
@@ -1,27 +1,14 @@
 use rdkafka::config::ClientConfig;
-use serde::Deserialize;
+use std::collections::HashMap;
+extern crate config;
 
-#[derive(Deserialize, Debug)]
-struct Config {
-    bootstrap_servers: String,
-    security_protocol: String,
-    sasl_mechanism: String,
-    sasl_username: String,
-    sasl_password: String,
-    topic: String,
-}
-
-pub fn read_config() -> (ClientConfig, String) {
+pub fn read_config() -> ClientConfig {
     let mut kafka_config = ClientConfig::new();
-    match envy::from_env::<Config>() {
-        Ok(config) => {
-            kafka_config.set("bootstrap.servers", &config.bootstrap_servers);
-            kafka_config.set("security.protocol", &config.security_protocol);
-            kafka_config.set("sasl.mechanism", &config.sasl_mechanism);
-            kafka_config.set("sasl.username", &config.sasl_username);
-            kafka_config.set("sasl.password", &config.sasl_password);
-            (kafka_config, config.topic)
-        }
-        Err(error) => panic!("Couldn't read config ({})", error),
-    }
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("kafka_config.toml")).unwrap();
+    
+    for (config_key, config_value) in &settings.try_into::<HashMap<String, String>>().unwrap() {
+        kafka_config.set(&config_key.replace("_", "."), &config_value);
+    };
+    return kafka_config;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod config_reader;
 
 fn main() {
     let config = config_reader::read_config();
-    println!("Config baby {:?}", config.0);
-    println!("TOPICO {:?}", config.1);
+    println!("{:?}", config);
 }
