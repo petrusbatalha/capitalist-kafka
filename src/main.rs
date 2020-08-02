2d62aec7d8dc7fdf6508b6a986547c42bb93ea14
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-02 14:48:18 -0300

    get high watermark.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::Consumer;
-use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
+use std::convert::TryInto;
 use std::time::Duration;
+use std::boxed::Box;
 mod config_reader;
 
 pub type TopicName = String;
 pub type TopicPartition = (TopicName, i32);
 pub type BrokerId = i32;
 
-#[derive(PartialEq, Serialize, Deserialize, Debug, Clone)]
-pub struct Partition {
-    pub id: i32,
-    pub leader: BrokerId,
-    pub replicas: Vec<BrokerId>,
-    pub isr: Vec<BrokerId>,
-}
-
-impl Partition {
-    fn new(
-        id: i32,
-        leader: BrokerId,
-        mut replicas: Vec<BrokerId>,
-        mut isr: Vec<BrokerId>,
-    ) -> Partition {
-        replicas.sort();
-        isr.sort();
-        Partition {
-            id,
-            leader,
-            replicas,
-            isr,
-        }
-    }
-}
-
-fn main() {
-    let mut mapito: HashMap<String, Partition> = HashMap::new(); 
+fn main() -> Result<(),Box<dyn std::error::Error>> {
     let config = config_reader::read_config();
     let consumer: StreamConsumer = config.0.create().expect("ERRITO");
 
@@ -44,18 +17,14 @@ fn main() {
     consumer.start();
     //let watermarks = consumer.fetch_watermarks(Some(""), 1, Duration::from_secs(1));
     //println!("{:?}", &watermarks);
-    let metadata = consumer.fetch_metadata(None, Duration::from_secs(1)).expect("errou");
+    let metadata = &consumer.fetch_metadata(None, Duration::from_secs(1)).expect("errou");
     for topic in metadata.topics() {
-        for p in topic.partitions() {
-            let partition = Partition::new(
-                p.id(),
-                p.leader(),
-                p.replicas().to_owned(),
-                p.isr().to_owned(),
-            );
-            print!("{:?}", partition);
+        let partitions: i32 = topic.partitions().len().try_into().unwrap();
+        for p in 0..partitions {
+          let watermarks = &consumer.fetch_watermarks(topic.name(), p, Duration::from_secs(1))?;
+          print!("{:?}", &watermarks);
         }
-        //consumer.fetch_watermarks(topic.name(), topic., timeout: T)
     }
+    Ok(())
 }
 
