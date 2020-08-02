03caa0c925e61ef82f7e1ae25cbe442fbb73db8d
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-02 15:08:18 -0300

    add tupple with partition and high watermark.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::time::Duration;
 use std::boxed::Box;
 mod config_reader;
 
-pub type TopicName = String;
-pub type TopicPartition = (TopicName, i32);
-pub type BrokerId = i32;
+pub struct Topic {
+    pub name: String,
+    pub watermarks: Vec<(i32, i64)>,
+}
 
 fn main() -> Result<(),Box<dyn std::error::Error>> {
     let config = config_reader::read_config();
@@ -19,11 +20,13 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
     //println!("{:?}", &watermarks);
     let metadata = &consumer.fetch_metadata(None, Duration::from_secs(1)).expect("errou");
     for topic in metadata.topics() {
+        let mut partition_high_watermarks: Vec<(i32, i64)> = Vec::new();
         let partitions: i32 = topic.partitions().len().try_into().unwrap();
         for p in 0..partitions {
-          let watermarks = &consumer.fetch_watermarks(topic.name(), p, Duration::from_secs(1))?;
-          print!("{:?}", &watermarks);
+          let high_watermarks = &consumer.fetch_watermarks(topic.name(), p, Duration::from_secs(1))?;
+          partition_high_watermarks.push((p, high_watermarks.1));
         }
+        print!("{:?}", partition_high_watermarks);
     }
     Ok(())
 }
