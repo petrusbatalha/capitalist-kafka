39180de1402fa7068f9749cf282406c93af1cb7a
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-13 17:10:14 -0300

    Multi-thread improvement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate num_cpus;
 use futures::stream::FuturesUnordered;
 use futures::StreamExt;
+use futures::TryStreamExt;
 use helpers::parser::parse_message;
 use helpers::reader::read_config;
-use helpers::utils::{OffsetRecord, Result};
+use helpers::utils::OffsetRecord;
+use log::{info, warn};
 use rdkafka::config::ClientConfig;
 use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::Consumer;
@@ -11,10 +13,7 @@ use rdkafka::message::{Message, OwnedMessage};
 use std::time::Duration;
 mod helpers;
 
-async fn fetch_highwatermarks(
-    config: ClientConfig,
-    owned_message: OwnedMessage,
-) -> Result<OffsetRecord> {
+fn fetch_highwatermarks(config: ClientConfig, owned_message: OwnedMessage) {
     let key = owned_message.key().unwrap_or(&[]);
     let payload = owned_message.payload().unwrap_or(&[]);
     match parse_message(key, payload) {
@@ -25,45 +24,46 @@ async fn fetch_highwatermarks(
             offset,
         }) => {
             let consumer: StreamConsumer = config.create().unwrap();
-            let high_watermarks = &consumer
-                .fetch_watermarks(&topic, partition, Duration::from_millis(1000))
-                .expect("TIME OUT FETCHING WATERMARKS");
-            Ok(OffsetRecord::GroupOffsetLag {
-                group: group,
-                topic: topic,
-                partition: partition,
-                offset: offset,
-                lag: high_watermarks.1 - offset,
-            })
+            match &consumer.fetch_watermarks(&topic, partition, Duration::from_millis(1000)) {
+                Ok(hwms) => {
+                    let group_offset_lag = OffsetRecord::GroupOffsetLag {
+                        group: group,
+                        topic: topic,
+                        partition: partition,
+                        offset: offset,
+                        lag: hwms.1 - offset,
+                    };
+                    println!("{:?}", group_offset_lag)
+                }
+                Err(e) => warn!("Error to process High Topic Watermarks, {:?}", e),
+            }
         }
-        Ok(_) => Ok(OffsetRecord::Metadata),
-        Err(e) => return Err(e),
+        _ => (),
     }
 }
 
 async fn consume(config: ClientConfig) {
     let consumer: StreamConsumer = config.create().unwrap();
-    consumer
-        .subscribe(&["__consumer_offsets"])
-        .expect("Can't subscribe to specified topic");
-    while let Some(message) = consumer.start().next().await {
-        match message {
-            Ok(message) => {
-                let owned_config = config.to_owned();
-                let owned_message = message.detach();
-                let lag = tokio::task::spawn_blocking(|| {
-                    fetch_highwatermarks(owned_config, owned_message)
-                }).await.expect("Impossible to calculate lag.");
-                println!("{:?}", lag.await);
-            }
-            Err(e) => println!("{:?}", e),
+    consumer.subscribe(&["__consumer_offsets"])
+            .expect("Can't subscribe to __consumer_offset topic. ERR: {}");
+    let stream_processor = consumer.start().try_for_each(|borrowed_message| {
+        let owned_config = config.to_owned();
+        async move {
+            let owned_message = borrowed_message.detach();
+            tokio::spawn(async move {
+                tokio::task::spawn_blocking(|| fetch_highwatermarks(owned_config, owned_message)).await.expect("Failed to calculate lag");
+            });
+            Ok(())
         }
-    }
+    });
+    info!("Starting event loop");
+    stream_processor.await.expect("stream processing failed");
+    info!("Stream processing terminated");
 }
 
 #[tokio::main]
 async fn main() {
-    (0..num_cpus::get() as usize)
+    (0..num_cpus::get())
         .map(|_| tokio::spawn(consume(read_config())))
         .collect::<FuturesUnordered<_>>()
         .for_each(|_| async { () })
