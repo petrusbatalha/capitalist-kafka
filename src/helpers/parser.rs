6d093f941b144662d28d2ebc53edb962c24a2cfe
Petrus Batalha <petrus.batalha1@ibm.com>
2020-08-13 12:10:12 -0300

    replace expect for ?

--- src/helpers/parser.rs
+++ src/helpers/parser.rs
@@ -7,7 +7,7 @@ use std::str;
 pub fn read_str<'a>(rdr: &'a mut Cursor<&[u8]>) -> Result<&'a str> {
     let len = (rdr.read_i16::<BigEndian>())? as usize;
     let pos = rdr.position() as usize;
-    let slice = str::from_utf8(&rdr.get_ref()[pos..(pos + len)]).expect("a");
+    let slice = str::from_utf8(&rdr.get_ref()[pos..(pos + len)])?;
     rdr.consume(len);
     Ok(slice)
 }
